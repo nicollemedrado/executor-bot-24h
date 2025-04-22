464938974093f2a1a74d585cd50ea751fe32f14d
nicollemedrado <atenasnicolle15@gmail.com>
2025-04-22 19:49:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,10 @@ import os
 import requests
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-def enviar_mensagem(mensagem):
-    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-    payload = {
-        "chat_id": TELEGRAM_CHAT_ID,
-        "text": mensagem,
-        "parse_mode": "HTML"
-    }
-    response = requests.post(url, data=payload)
-    print("Código de resposta:", response.status_code)
-    print("Conteúdo:", response.text)
+def descobrir_chat_id():
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/getUpdates"
+    response = requests.get(url)
+    print(response.json())
 
-enviar_mensagem("✅ Teste de envio do Bot Executor")
+descobrir_chat_id()
